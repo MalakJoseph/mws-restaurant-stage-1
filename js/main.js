ad5607b0834922aa72cb4ac8a69cdc2af058dc0d
Malak Joseph <malakjoseph2010@gmail.com>
2018-07-04 20:52:24 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -215,7 +215,7 @@ addMarkersToMap = (restaurants = self.restaurants) => {
  * Add the service worker
  */
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/restaurant-review-app/sw.js'})
+  navigator.serviceWorker.register('/restaurant-review-app/sw.js')
     .then(reg => {
       //Registeration Worked!
       if (reg.installing) {
